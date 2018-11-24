91babf87aefbf586c6966a98483c18de85ff722e
Javier Pastor Pérez <Javier Pastor Prez@192.168.3.133>
2018-11-24 13:03:54 +0100

    Ayuda

---
+++ src/main/java/upv/etsinf/cognispatium/web/AyudaController.java
@@ -0,0 +1,89 @@
+package upv.etsinf.cognispatium.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import upv.etsinf.cognispatium.service.SimpleServicioManager;
+import upv.etsinf.cognispatium.service.SimpleSolicitudManager;
+import upv.etsinf.cognispatium.service.SimpleUsuarioManager;
+import upv.etsinf.cognispatium.domain.Cliente;
+import upv.etsinf.cognispatium.domain.Consulta;
+import upv.etsinf.cognispatium.domain.ConsultaUrgente;
+import upv.etsinf.cognispatium.domain.EstadoConsulta;
+import upv.etsinf.cognispatium.domain.EstadoPresupuesto;
+import upv.etsinf.cognispatium.domain.EstadoSolicitud;
+import upv.etsinf.cognispatium.domain.Presupuesto;
+import upv.etsinf.cognispatium.domain.Profesional;
+import upv.etsinf.cognispatium.domain.Registro;
+import upv.etsinf.cognispatium.domain.Respuesta;
+import upv.etsinf.cognispatium.domain.Servicio;
+import upv.etsinf.cognispatium.domain.Solicitud;
+import upv.etsinf.cognispatium.domain.Usuario;
+import upv.etsinf.cognispatium.service.SimpleClienteManager;
+import upv.etsinf.cognispatium.service.SimpleConsultaManager;
+import upv.etsinf.cognispatium.service.SimpleConsultaUrgenteManager;
+import upv.etsinf.cognispatium.service.SimpleProfesionalManager;
+import upv.etsinf.cognispatium.service.SimpleRegistroManager;
+import upv.etsinf.cognispatium.service.SimpleRespuestaManager;
+import upv.etsinf.cognispatium.service.SimplePresupuestoManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+@Controller
+public class AyudaController {
+
+	@Autowired
+	private SimpleServicioManager servicioManager;
+	
+
+	/** Logger for this class and subclasses */
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	@RequestMapping("/ayuda.htm")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Map<String, Object> myModel = new HashMap<String, Object>();
+
+		ModelAndView mav = new ModelAndView("ayuda", "model", myModel);
+		
+		return mav;
+
+	}
+
+	@PostMapping("/ayuda.htm")
+	protected ModelAndView editar(@RequestParam Map<String, String> reqPar) throws Exception {
+		Map<String, Object> myModel = new HashMap<String, Object>();
+
+		ModelAndView mav = new ModelAndView("ayuda", "model", myModel);
+		
+		return mav;
+		
+	}
+
+	public void deleteServicioManager() {
+
+	}
+
+	public void setServicioManager(SimpleServicioManager servicioManager) {
+		this.servicioManager = servicioManager;
+	}
+	
+
+}
